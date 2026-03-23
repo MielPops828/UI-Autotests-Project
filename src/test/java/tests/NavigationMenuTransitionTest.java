d04938312c5742fd36bfc8e8112c9c6a10b2f67b
Alexandr Merkulov <merku0103@mail.ru>
2026-03-23 12:13:11 +0400

    add allure report

--- src/test/java/tests/NavigationMenuTransitionTest.java
+++ src/test/java/tests/NavigationMenuTransitionTest.java
@@ -1,12 +1,17 @@
 package tests;
 
+import io.qameta.allure.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import utils.ParameterProvider;
 
+@Epic("Автоматизация страницы Way2Automation")
+@Feature("Главная страница")
+@Story("Переход по меню навигации на другие страницы")
 public class NavigationMenuTransitionTest extends BaseTest{
     @Test
+    @Severity(SeverityLevel.NORMAL)
     public void testNavigationMenuTransition(){
         HomePage home = new HomePage(getDriver()).openPage().navigateTransition();
         Assert.assertTrue(home.hasTransition(ParameterProvider.get("transition.url")));
